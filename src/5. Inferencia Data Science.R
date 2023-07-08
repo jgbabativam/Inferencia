69d8078d45709d1ddb066c805aa5f311448d96dc
gbabativam <gbabativam@gmail.com>
2023-07-08 07:51:43 -0500

    computacion

--- src/5. Inferencia Data Science.R
+++ src/5. Inferencia Data Science.R
@@ -18,3 +18,17 @@ datos <- data.frame(
              53.0, 53.8, 51.6, 53.6, 42.9, 52.0, 55.1, 57.1, 62.8, 54.8)) |> 
       mutate(condicion = 2)  
   )
+
+#-------------------------------------
+# INTERVALO DE CONFIANZA PARA M1 - M2
+
+glimpse(datos)  #ver la estructura de los datos
+
+datos$condicion <- factor(datos$condicion,
+                          levels = c(1, 2),
+                          labels = c("Cond1", "Cond2"))
+
+
+
+
+
